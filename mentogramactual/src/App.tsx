ca8314c0109e9314d2ded8431866432fd04ae3d5
Varun R Mallya <varunrmallya@gmail.com>
2023-12-06 14:22:24 +0530

    lint

--- mentogramactual/src/App.tsx
+++ mentogramactual/src/App.tsx
@@ -1,5 +1,5 @@
 import { Button } from "@/components/ui/button"
-import React, { useState } from 'react';
+import { useState } from 'react';
 import Mentorlogin from './mentorlogin';
 
 function App() {
